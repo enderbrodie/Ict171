8d0412567838e51386b6fa4dd152e2be452c8c41
Deadbtb <54665089+enderbrodie@users.noreply.github.com>
2025-05-27 11:20:50 +0800

    More simple changes

--- the forest python.py
+++ the forest python.py
@@ -1,23 +1,27 @@
-def choice():
-    rep = input()
-    if rep == '1':
-        print(".")
-    
-    if rep == '2':
-        print("You turn around and someone smashed a bottle over you head your DEAD")
-
-
 L = 0
 print("Welcome to the forest")
 print("Use the number to help you navigate this game")
 print("You off to side of the road and stare into the wood as if it wants you to enter. You get out of your car")
 print("1.Forward 2.Go back to the car")
-while L == 0:
-    rep = input()
-    if rep == '1':
+rep = input()
+if rep == '1':
         print("you move forward\n")
         print("There are still only tree blocking your path but you can still get around them")
-    if rep == '2':
-        print("You turn around and someone smashed a bottle over you head your DEAD")
-        break
-    print("1.Forward 2.walk round")
+        rep == 0
+        print("1.Forward 2.walk round")
+        rep = input()
+        if rep == '1':
+            print("you reach an old run down shack and as you look you notice a man")
+            print("1.approach 2.look around")
+            rep == 0
+            rep = input()
+            if rep == '1':
+                print("you move forward towards the man he grabs his gun and shoots you dead on the spot police will never find your body.THE END\n")
+            if rep == '2':
+                print("You notice a loaded gun sitting near the kitchen table")
+                rep == 0
+                rep = input()
+                print("1.Forward 2.walk round")
+            
+if rep == '2':
+    print("You turn around and someone smashed a bottle over you head your DEAD")
